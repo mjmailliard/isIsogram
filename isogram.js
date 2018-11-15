ddc1e515827627e10f6361c15f6b062b2f402049
Mike <mjmailliard@gmail.com>
2018-11-14 21:10:01 -0700

    solution

--- isogram.js
+++ isogram.js
@@ -2,7 +2,27 @@
 
 
 export const isIsogram = (string) => {
+    
     if(string === '')
         return true
 
-} *
+   const lowerCaseString = string.toLowerCase()
+   const noDashString = lowerCaseString.replace(/[-]/g, '')
+   const cleanString = noDashString.replace(/[' ']/g, '')
+
+   const stringArray = cleanString.split('')
+   
+   const checkDupes = ((current, index) => stringArray.indexOf(current) === index)
+   
+   if(stringArray.every(checkDupes)) {
+    return true
+   }else{
+    return false
+   }
+
+
+
+
+    
+    
+} 
